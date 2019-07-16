07c1261525c54e767d016da7a218a5ca88475635
HAOYUatHZ <haoyu@protonmail.com>
2019-07-16 18:02:40 +0800

    update ignore

--- vineetjohn/solutions/src/example.rs
+++ vineetjohn/solutions/src/example.rs
@@ -1,5 +1,5 @@
 #[allow(dead_code)]
-pub fn check_sums(a: i32,  b: i32) -> i32 {
+pub fn add(a: i32,  b: i32) -> i32 {
     return a + b
 }
 
@@ -9,7 +9,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_check_sums() {
-        assert_eq!(check_sums(1, 2), 3);
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
     }
 }
